8be087a971fc89d40029d4b2dc8e86b8268c64ed
liwenjie <lliwenjie@guanmai.cn>
2023-03-31 18:33:24 +0800

    commit

--- deploy/mysql/init/start.sh
+++ deploy/mysql/init/start.sh
@@ -2,4 +2,10 @@
 
 cat /etc/mysql/mysql.conf.d/source/mysqld.cnf >  /etc/mysql/mysql.conf.d/mysqld.cnf
 
+cat /etc/init.d/init/3-mysql_init.sh > /etc/init.d/3-mysql_init.sh
+
+chmod 0755 /etc/init.d/3-mysql_init.sh
+
+sh /etc/init.d/3-mysql_init.sh
+
 /entrypoint.sh mysqld
---
+++ deploy/mysql/start.sh
@@ -0,0 +1,16 @@
+# mysql搭建主从
+docker exec -it mysql_master /bin/bash /etc/init.d/3-mysql_init.sh
+docker exec -it mysql_slave1 /bin/bash /etc/init.d/3-mysql_init.sh
+docker exec -it mysql_slave2 /bin/bash /etc/init.d/3-mysql_init.sh
+# 生成ssh key
+docker exec -it mysql_master /bin/bash /etc/init.d/script/ssh_generate_key.sh -y
+docker exec -it mysql_slave1 /bin/bash /etc/init.d/script/ssh_generate_key.sh -y
+docker exec -it mysql_slave2 /bin/bash /etc/init.d/script/ssh_generate_key.sh-y
+docker exec -it mha_manager /bin/bash /etc/init.d/script/ssh_generate_key.sh -y
+# 授权ssh key
+docker exec -it mysql_master /bin/bash /etc/init.d/script/ssh_auth_keys.sh
+docker exec -it mysql_slave1 /bin/bash /etc/init.d/script/ssh_auth_keys.sh
+docker exec -it mysql_slave2 /bin/bash /etc/init.d/script/ssh_auth_keys.sh
+docker exec -it mha_manager /bin/bash /etc/init.d/script/ssh_auth_keys.sh
+# 启动 MHA
+docker exec mha_manager /bin/bash /etc/init.d/mha_manager.sh
